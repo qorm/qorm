8fc6dd43ae55f0fde2d2d14198fd27418931dbac
qorm <github@qorm.com>
2026-07-27 01:00:22 -0400

    chore(mdsite): embed GTM and AdSense in generated docs pages

--- internal/mdsite/site.go
+++ internal/mdsite/site.go
@@ -563,6 +563,15 @@ func pageHTML(title, lang, siteName, head, langSwitch, nav, body string) string 
 	return fmt.Sprintf(`<!doctype html>
 <html lang="%s">
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K3FQCHWD');</script>
+<!-- End Google Tag Manager -->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1529418578004538"
+     crossorigin="anonymous"></script>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>%s · QORM</title>
@@ -662,6 +671,10 @@ func pageHTML(title, lang, siteName, head, langSwitch, nav, body string) string 
      if(pref&&pref!==cur){var alt=document.querySelector('header a[data-lang="'+pref+'"]');if(alt)location.replace(alt.getAttribute('href'));}
    }catch(e){}})();
 </script>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3FQCHWD"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 </body>
 </html>
 `, lang, html.EscapeString(title), head, verBadge, html.EscapeString(siteName), homeLabel, navLinks, patronLabel, patronLabel, langSwitch, nav, body)
